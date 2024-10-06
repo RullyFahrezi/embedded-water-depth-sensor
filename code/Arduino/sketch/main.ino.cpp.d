1fbf4501e4630f013b5873d8b4191b6b46555dd2
RullyFahrezi <165125337+RullyFahrezi@users.noreply.github.com>
2024-10-07 01:57:24 +0800

    add file sketch

---
+++ code/Arduino/sketch/main.ino.cpp.d
@@ -0,0 +1,16 @@
+C:\Users\ACERGK~1\AppData\Local\Temp\arduino_build_249495\sketch\main.ino.cpp.o: \
+ C:\Users\ACERGK~1\AppData\Local\Temp\arduino_build_249495\sketch\main.ino.cpp \
+ C:\Users\Acer\ Gk\Documents\ArduinoData\packages\arduino\hardware\avr\1.8.6\cores\arduino/Arduino.h \
+ C:\Users\Acer\ Gk\Documents\ArduinoData\packages\arduino\hardware\avr\1.8.6\cores\arduino/binary.h \
+ C:\Users\Acer\ Gk\Documents\ArduinoData\packages\arduino\hardware\avr\1.8.6\cores\arduino/WCharacter.h \
+ C:\Users\Acer\ Gk\Documents\ArduinoData\packages\arduino\hardware\avr\1.8.6\cores\arduino/WString.h \
+ C:\Users\Acer\ Gk\Documents\ArduinoData\packages\arduino\hardware\avr\1.8.6\cores\arduino/HardwareSerial.h \
+ C:\Users\Acer\ Gk\Documents\ArduinoData\packages\arduino\hardware\avr\1.8.6\cores\arduino/Stream.h \
+ C:\Users\Acer\ Gk\Documents\ArduinoData\packages\arduino\hardware\avr\1.8.6\cores\arduino/Print.h \
+ C:\Users\Acer\ Gk\Documents\ArduinoData\packages\arduino\hardware\avr\1.8.6\cores\arduino/Printable.h \
+ C:\Users\Acer\ Gk\Documents\ArduinoData\packages\arduino\hardware\avr\1.8.6\cores\arduino/USBAPI.h \
+ C:\Users\Acer\ Gk\Documents\ArduinoData\packages\arduino\hardware\avr\1.8.6\cores\arduino/Arduino.h \
+ C:\Users\Acer\ Gk\Documents\ArduinoData\packages\arduino\hardware\avr\1.8.6\variants\mega/pins_arduino.h \
+ C:\Program\ Files\WindowsApps\ArduinoLLC.ArduinoIDE_1.8.57.0_x86__mdqgnx93n4wtt\libraries\LiquidCrystal\src/LiquidCrystal.h \
+ C:\Users\Acer\ Gk\Documents\ArduinoData\packages\arduino\hardware\avr\1.8.6\cores\arduino/Print.h \
+ C:\Users\Acer\ Gk\Documents\Arduino\libraries\NewPing\src/NewPing.h
